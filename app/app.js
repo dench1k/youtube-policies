3d7cf4e95912d0774b14b6725cef385ebed0da3c
Den Moskvitin <denmoskvitin@gmail.com>
2020-04-03 21:16:43 +0300

    Test getting elements

--- app/app.js
+++ app/app.js
@@ -2,9 +2,24 @@ const puppeteer = require("puppeteer");
 
 (async () => {
   const browser = await puppeteer.launch();
+  // const browser = await puppeteer.launch({
+  //   headless: false,
+  //   slowMo: 250, // slow down by 250ms
+  // });
   const page = await browser.newPage();
-  await page.goto("https://example.com");
-  await page.screenshot({ path: "example.png" });
+  await page.setViewport({ width: 1280, height: 1024 });
+  await page.goto(
+    "https://accounts.google.com/ServiceLogin?service=youtube&uilel=3&passive=true&continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Faction_handle_signin%3Dtrue%26app%3Ddesktop%26hl%3Dru%26next%3Dhttps%253A%252F%252Fwww.youtube.com%252F&hl=ru&ec=65620"
+  );
+
+  const h1 = await page.evaluate(
+    () => document.querySelector("h2").textContent
+  );
+  const link = await page.$("input[type=email]");
+  const text = await (await link.getProperty("id")).jsonValue();
+
+  console.log(h1);
+  console.log(text);
 
   await browser.close();
 })();
