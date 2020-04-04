dcfc4ff46666e24645e82be97b9cf92744bafa29
Den Moskvitin <denmoskvitin@gmail.com>
2020-04-04 22:41:51 +0300

    Try to login in front

--- app/app.js
+++ app/app.js
@@ -1,25 +1,49 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch();
-  // const browser = await puppeteer.launch({
-  //   headless: false,
-  //   slowMo: 250, // slow down by 250ms
-  // });
+  //const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 250, // slow down by 250ms
+  });
   const page = await browser.newPage();
-  await page.setViewport({ width: 1280, height: 1024 });
-  await page.goto(
-    "https://accounts.google.com/ServiceLogin?service=youtube&uilel=3&passive=true&continue=https%3A%2F%2Fwww.youtube.com%2Fsignin%3Faction_handle_signin%3Dtrue%26app%3Ddesktop%26hl%3Dru%26next%3Dhttps%253A%252F%252Fwww.youtube.com%252F&hl=ru&ec=65620"
-  );
+  const navigationPromise = page.waitForNavigation();
 
-  const h1 = await page.evaluate(
-    () => document.querySelector("h2").textContent
-  );
-  const link = await page.$("input[type=email]");
-  const text = await (await link.getProperty("id")).jsonValue();
+  await page.goto("https://accounts.google.com/");
 
-  console.log(h1);
-  console.log(text);
+  await navigationPromise;
+
+  await page.waitForSelector('input[type="email"]');
+  await page.click('input[type="email"]');
+
+  await navigationPromise;
+
+  //TODO : change to your email
+  await page.type('input[type="email"]', "login@gmail.com");
+  await page.waitForSelector("#identifierNext");
+  await page.click("#identifierNext");
+
+  await page.waitFor(500);
+
+  await page.waitForSelector('input[type="password"]');
+  await page.click('input[type="email"]');
+  await page.waitFor(500);
+  //TODO : change to your password
+  await page.type('input[type="password"]', "password");
+
+  await page.waitForSelector("#passwordNext");
+  await page.click("#passwordNext");
+
+  await navigationPromise;
+
+  // const h1 = await page.evaluate(
+  //   () => document.querySelector("h2").textContent
+  // );
+  //const link = await page.$("input[type=email]");
+  //const text = await (await link.getProperty("id")).jsonValue();
+
+  //console.log(h1);
+  //console.log(text);
 
   await browser.close();
 })();
