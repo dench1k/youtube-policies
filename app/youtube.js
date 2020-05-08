9f43ca0bd1e561fc34fbeeb36023c4b35cac89d0
Den Moskvitin <denmoskvitin@gmail.com>
2020-05-08 14:14:45 +0300

    Add check method

--- app/youtube.js
+++ app/youtube.js
@@ -10,6 +10,20 @@ puppeteer.use(StealthPlugin());
 
 const BASE_URL = "https://www.youtube.com/music_policies?nv=1";
 const LOGIN_URL = "https://accounts.google.com/";
+const tracklist = [
+  {
+    artist: "Loadstar",
+    title: "Once Again",
+  },
+  {
+    artist: "Milkyway",
+    title: "Fairy Tale",
+  },
+  {
+    artist: "Random Movement",
+    title: "Lake Escape",
+  },
+];
 
 let browser = null;
 let page = null;
@@ -65,11 +79,113 @@ const youtube = {
     await fillPassword(password);
     await clickPasswordNext();
     console.log("logged");
+    await page.goto(BASE_URL);
+    console.log("redirected");
   },
+  check: async () => {
+    for ([index, track] of tracklist.entries()) {
+      console.log(index, track);
 
+      await page.evaluate(() => {
+        document.querySelector('input[maxlength="80"]').click();
+      });
+
+      await page.waitFor(1000);
+      await page.type(
+        'input[maxlength="80"]',
+        `${tracklist[index].artist} - ${tracklist[index].title}`,
+        {
+          delay: 50,
+        }
+      );
+      console.log("typed");
+      await page.waitFor(3000);
+
+      await page.evaluate(() => {
+        document.querySelector(".search-icon").click();
+      });
+      console.log("clicked search icon");
+      //await page.waitFor(10000);
+
+      // search results
+      await page.waitForSelector(".track-list.sorting");
+      await page.waitFor(1000);
+      let resultsArray = await page.$$(".track-list > li");
+      let results = [];
+
+      // isBanned
+      // .track-content .asset-not-available-text
+
+      // isOk
+      // !(.track-content .asset-not-available-text)
+
+      // isMissing
+      // .track-content.no-results
+
+      // for infinite scroll
+      // let lastResultArrayLength = 0;
+      // while (resultsArray.length < count) {
+      //   await page.evaluate(`window.scrollTo(0, document.body.scrollHeight)`);
+      //   await page.waitFor(3000);
+
+      //   resultsArray = await page.$$(".track-list > li");
+
+      //   if (lastResultArrayLength === resultsArray.length) break;
+
+      //   lastResultArrayLength = resultsArray.length;
+      // }
+      console.log("Entering for loop");
+      for (let [resultIndex, resultElement] of resultsArray.entries()) {
+        let artist = await resultElement.$eval(
+          ".audiolibrary-column-artist",
+          (element) => element.innerText
+        );
+        let title = await resultElement.$eval(
+          ".audiolibrary-column-title",
+          (element) => element.innerText
+        );
+        console.log(`=========`);
+        console.log(`Elem : ${artist} - ${title}`);
+        console.log(`=========`);
+        console.log(
+          `Tracklist : ${tracklist[index].artist} - ${tracklist[index].title}`
+        );
+        console.log(`=========`);
+        if (
+          artist === tracklist[index].artist &&
+          title === tracklist[index].title
+        ) {
+          await page.evaluate((i) => {
+            let num = i + 1;
+            console.log(num);
+            document
+              .querySelector(
+                `.track-list li:nth-child(${num}) .audiolibrary-track-head`
+              )
+              .click();
+          }, resultIndex);
+
+          console.log("clicked li");
+          await page.waitFor(1000);
+        }
+        results.push(title);
+      }
+
+      console.log(results);
+      await page.screenshot({
+        path: `testresult${index}.png`,
+        fullPage: true,
+      });
+      console.log("screenshoted");
+      await page.evaluate(() => {
+        document.querySelector('input[maxlength="80"]').value = "";
+      });
+      await page.waitFor(1000);
+    }
+  },
   end: async () => {
     await browser.close();
   },
 };
 
-module.exports = twitter;
+module.exports = youtube;
